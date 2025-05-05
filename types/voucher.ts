dd7e841ca324399e71ed8301572bb4199b030cca
jeongmin07262 <dlwjdals0726@gmail.com>
2025-05-05 10:19:21 +0900

    feat: Voucher 타입 정의 추가

---
+++ types/voucher.ts
@@ -0,0 +1,13 @@
+export interface Voucher {
+    id: number
+    department: string
+    title: string
+    description: string
+    deadline: string
+    amount: string
+    color: string
+    icon: string
+    image: string
+    category: string
+  }
+  
