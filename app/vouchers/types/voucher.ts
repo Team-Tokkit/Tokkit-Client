5ed481da581736662ac7efc07f5a902dbb6dc012
jeongmin07262 <dlwjdals0726@gmail.com>
2025-05-13 12:15:09 +0900

    refactor: 백엔드 응답 DTO 변경에 따라 types 수정

---
+++ app/vouchers/types/voucher.ts
@@ -0,0 +1,35 @@
+import { StoreResponse } from '@/types/store'
+export interface Voucher {
+  id: number
+  name: string
+  description: string
+  price: number
+  originalPrice?: number
+  validDate: string
+  contact?: string
+  remainingCount?: number
+  imageUrl: string
+  merchant?: string
+}
+
+export interface VoucherDetail {
+  id: number
+  name: string
+  price: number
+  originalPrice: number
+  remainingCount: number
+  validDate: string // 백엔드에서 yyyy-MM-dd 형식으로 보내므로 string
+  detailDescription: string
+  refundPolicy: string
+  contact: string | null
+  imageUrl: string | null
+  stores: {
+    content: StoreResponse[]
+    totalPages: number
+    totalElements: number
+    size: number
+    number: number
+    first: boolean
+    last: boolean
+  }
+}
