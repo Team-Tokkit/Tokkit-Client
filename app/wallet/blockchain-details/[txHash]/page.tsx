ff9e9aab6ad483751beabbedaf9298a91d1cf02f
noeyoes <>
2025-05-26 22:08:08 +0900

    feat: 블록체인 상세페이지 구현

---
+++ app/wallet/blockchain-details/[txHash]/page.tsx
@@ -0,0 +1,49 @@
+"use client"
+
+import { useEffect, useState } from "react"
+import { useParams } from "next/navigation"
+import { useToast } from "@/hooks/use-toast"
+import { fetchTxDetail } from "../../api/fetch-txhash-details"
+import BlockchainDetailsLoading from "../components/BlockchainDetailsLoading"
+import { HeaderSection } from "../components/HeaderSection"
+import {TransactionDetailCard} from "@/app/wallet/blockchain-details/components/TransactionDetailCard";
+import {BlockchainTransaction} from "@/app/wallet/blockchain-details/types/blockchain";
+import MascotCardSection from "@/app/wallet/blockchain-details/components/MascotCardSection";
+
+export default function BlockchainDetailsPage() {
+    const { txHash } = useParams()
+    const { toast } = useToast()
+    const [transaction, setTransaction] = useState<BlockchainTransaction | null>(null)
+
+    useEffect(() => {
+        if (!txHash || typeof txHash !== "string") {
+            toast({ title: "txHash 누락", description: "유효한 트랜잭션 해시가 필요합니다." })
+            return
+        }
+
+        const fetchData = async () => {
+            try {
+                const tx = await fetchTxDetail(txHash)
+                setTransaction(tx)
+            } catch (error: any) {
+                toast({ title: "트랜잭션 조회 실패", description: error.message })
+            }
+        }
+
+        fetchData()
+    }, [txHash])
+
+    if (!transaction) {
+        return <BlockchainDetailsLoading />
+    }
+
+    return (
+        <div className="min-h-screen bg-[#F9FAFB] max-w-md mx-auto">
+            <HeaderSection />
+            <div className="p-5">
+                <MascotCardSection />
+                <TransactionDetailCard transaction={transaction} />
+            </div>
+        </div>
+    )
+}
